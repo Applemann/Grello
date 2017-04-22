a2df34b98ca4e876c3949e2f02e5ef5ec1ca886d
Martin Jablečník <martin.jablecnik@email.cz>
2017-04-22 14:36:15 +0200

    added redis

--- server.py
+++ server.py
@@ -5,47 +5,64 @@ from flask import render_template
 import urllib2, json
 import utils
 from utils import get_token, for_print
+from redis import Redis
 
+redis = Redis()
+token = get_token()
 
 class Issue(object):
-    def __init__(self, title, body, repository):
+    def __init__(self, id, title, body, repository):
+        self.id=id
         self.title=title
         self.body=body
         self.repository=repository
 
-    
-def getAllIssues(token):
+def loadAllIssues():
     response = urllib2.urlopen('https://api.github.com/issues?access_token='+token)
     issues = json.loads(response.read())
-    retIssues = []
     for issue in issues:
-        retIssues.append(
+        redis.rpush("inbox", issue['id'])
+        redis.hmset( issue['id'], 
+                    {
+                        'id': issue['id'],
+                        'title': issue['title'],
+                        'body': issue['body'],
+                        'repository': issue['repository'],
+                    }
+                )
+        
+    
+def getIssues(collumn):
+    issues = []
+    issue_ids = redis.lrange(collumn, 0, -1)
+
+    for issue_id in issue_ids:
+        issue = redis.hgetall(issue_id)
+        issues.append(
             Issue(
+                issue['id'], 
                 issue['title'], 
                 issue['body'], 
                 issue['repository']
             ) 
         )
 
-    return retIssues
+    return issues
 
+
+#loadAllIssues()
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    
-    inbox_col = []
-    all_issues = getAllIssues(utils.get_token())
-    for issue in all_issues:
-        inbox_col.append(issue.title)
-
+    inbox_issues = getIssues('inbox')
 
     board = (
-        ("low-priority", ['item1', 'item2', 'item3']),
-        ('inbox', inbox_col),
-        ('high-priority', ['item1', 'item2', 'item3']),
-        ('inprogress', ['item1', 'item2', 'item3']),
-        ('done', ['item1', 'item2', 'item3']),
+        ("low-priority", []),
+        ('inbox', inbox_issues),
+        ('high-priority', []),
+        ('inprogress', []),
+        ('done', []),
     )
 
     return render_template('index.html', board=board)

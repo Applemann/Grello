5b6c7fefd0c4deb1f40d1403b46b54e9fa9eb41e
Martin Jablečník <martin.jablecnik@email.cz>
2017-05-02 21:59:37 +0200

    added removing closed issues

--- server.py
+++ server.py
@@ -41,10 +41,10 @@ PROJECTS=('erp', 'docker', 'bwt', 'backend', 'TeamZeus-Ansible', 'soap-bridge', 
 BACKGROUND_COLORS=('#6fd6ed', '#69e571', '#f453e7', '#f4383b', '#f7c53d', '#c95afc', '#d2f243', '#49f4b3', '#083887')
 COLORS=('#000000', '#000000', '#000000', '#000000', '#000000', '#000000', '#000000', '#000000', '#000000', '#ffffff')
 
-IssueFields = ('id', 'title', 'body', 'repository', 'html_url', 'repository_url')
+IssueFields = ('id', 'title', 'body', 'repository', 'html_url', 'repository_url', 'state')
 class Issue(object):
-    def __init__(self, issues):
-        for k, v in issues.iteritems():
+    def __init__(self, values):
+        for k, v in values.iteritems():
             self.__dict__[k] = v
         self.set_repository()
         self.set_colors()
@@ -70,13 +70,14 @@ def loadAllIssues():
 
     for issue in issues:
         if not str(issue['id']) in redis_issues:
-            redis.rpush(Columns.INBOX, issue['id'])
-            redis.hmset( issue['id'], {k: issue[k] for k in IssueFields} )
+            redis.rpush(Columns.INBOX, str(issue['id']))
+            redis.hmset( str(issue['id']), {k: issue[k] for k in IssueFields} )
+
         
     
-def getIssues(column):
+def getIssues(columnId):
     issues = []
-    issue_ids = redis.lrange(column, 0, -1)
+    issue_ids = redis.lrange(columnId, 0, -1)
 
     for issue_id in issue_ids:
         issue = redis.hgetall(issue_id)
@@ -85,14 +86,24 @@ def getIssues(column):
     return issues
 
 
+def removeClosedIssues():
+    issues = []
+    for column in getColumns():
+        issues = getIssues(column.id)
+        for issue in issues:
+            if issue.state == 'close':
+                redis.lrem(column, issue)
+
+
 
 #redis.flushdb()
+removeClosedIssues()
 loadAllIssues()
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    column = lambda x: (x, getIssues(x.id))
+    column = lambda x: (x, getIssues(x))
     board = (
         column(Columns.INBOX),
         column(Columns.LOW_PRIORITY),
